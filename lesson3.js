d5f49cff5d98133126615a2f18fe73905dce65fb
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-05-27 14:45:46 +0500

    lesson3 update

--- lesson3.js
+++ lesson3.js
@@ -1,6 +1,22 @@
+// Question1
 function temperature () {
     let tempindegree = parseFloat (document.getElementById("temp").value);
     let result = (tempindegree*9/ 5) + 32;
     
     document.getElementById("para1").innerHTML= `The temperature ${tempindegree} degree is equal to temperature ${result}F `;
 }
+
+// Question2
+let worldCupYears= " ";
+
+for (let i=2022; i<=2050; i+=4){
+    worldCupYears+=`<li>${i}</li>`;
+    
+}
+
+document.getElementById("worldCup").innerHTML = `${worldCupYears}`;
+
+
+// Question3
+
+
